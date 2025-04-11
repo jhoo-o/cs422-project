23893efb80f753aa818ae18402cdf65ce14c3285
Matthew Danque <mattdanque@gmail.com>
2025-04-10 22:21:59 -0500

    Finished Edit Card Information Page

--- src/pages/settingsPages/creditCardInfo.js
+++ src/pages/settingsPages/creditCardInfo.js
@@ -1,14 +1,23 @@
 import SettingsBar from "../../components/settingsTopBar";
-import {Typography, Container} from "@mui/material";
+import {Typography, Container, Button, TextField, Stack} from "@mui/material";
 
 const CreditCardInfo = () => {
     return (
         <>
             <SettingsBar previousDest={"/Settings"}/>
-            <Container sx={{flexGrow: 1}}>
-                <Typography variant="h3" sx={{flexGrow: 1, color:"blue"}}>
-                    Set Credit Card Information
-                </Typography>
+            <Container sx={{height:"40%", width:"50%", backgroundColor:'#e8f1ff', mt:15,
+                justifyContent:"center", flexGrow: 1, boxShadow: 10, borderRadius: 7, padding: 5, paddingBottom: 5}}>
+                <Container fixed sx={{flexGrow: 1, width: "100%", justifyContent:"center"}}>
+                    <Typography variant="h5">
+                        Edit Card Information
+                    </Typography>
+                    <Stack sx={{display: "flex", flexGrow: 1, justifyContent:"left"}}>
+                        <TextField variant="standard" label = "Name"/>
+                        <TextField variant="standard" label = "Credit Card #"/>
+                        <TextField variant="standard" label = "CVV"/>
+                        <Button variant="outlined" sx={{marginTop: 3, fontWeight:"bold"}}>Submit</Button>
+                    </Stack>
+                </Container>
             </Container>
         </>
     );
