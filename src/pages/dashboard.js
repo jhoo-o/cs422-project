781eab13134d41ce5d54fad191eef5e1e14ceccc
jhoo-o <ho177133@gmail.com>
2025-04-13 21:59:36 -0500

    spent like 4 hours trying to rotate a square and still haven't solved it, gonna just make the individual images

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {useNavigate} from "react-router-dom";
-import {AppBar, Box, List, ListItemText, ListItemButton, Divider, Toolbar, 
-    IconButton, Icon, SvgIcon, ListItem, ListItemAvatar, Avatar, Stack, Typography} from "@mui/material";
+import {AppBar, Box, List, ListItemText, ListItemButton, 
+    Divider, Toolbar, IconButton, Icon, SvgIcon, ListItem, 
+    ListItemAvatar, Avatar, Stack, Typography} from "@mui/material";
 import {ArrowBack, Settings} from '@mui/icons-material';
 import picture from './added_assets/circle.png';
 import { Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import events from "./tasks_for_dashboard/events";
 
 
 const localizer = momentLocalizer(moment)
@@ -27,10 +29,11 @@ const Dashboard = () => {
         navigate("/Settings");
     }
     
+    const [eventsData, setEventsData] = useState(events);
 
     return (
         <div>
-            {/*WHY ARE COMMENTS LIKE THIS, anyways stole this box from matthew*/}
+            {/*WHY ARE COMMENTS LIKE THIS, anyways stole this box from matthew, store settings bar*/}
             <Box sx={{flexGrow: 1}}>
                 <AppBar position="static">
                     <Toolbar>
@@ -49,9 +52,10 @@ const Dashboard = () => {
                     </Toolbar>
                 </AppBar>
             </Box>
+            {/* Calendar box*/}
             <Box sx={{flexGrow: 1}}>
                 <Calendar
-                views = {['day', 'month']}
+                    views = {['day', 'week', 'month']}
                     localizer = {localizer}
                     selectable
                     defaultDate = {new Date()}
@@ -59,11 +63,42 @@ const Dashboard = () => {
                     style = {{height:'50vh', width : '100vw'}}
                     startAccessor="start"
                     endAccessor="end"
+                    events = {eventsData}
                 />
             </Box>
-            <Box sx={{flexGrow: 1, width: 1}}>
+            {/* Stores user task list*/}
+            <Box sx={{flexGrow: 1, width: 1, overflow: 'scroll'}}>
                 <List sx ={{flexGrow: 1}}>
-                    <ListItem sx = {{borderColor:'#000000'}}>
+                <ListItem sx = {{borderColor:'#000000', border: '1px solid black'}}>
+                        {/* <ListItemAvatar>
+                            <Avatar alt ="Espurr" src = {picture}/>
+                        </ListItemAvatar> */}
+                        <Box width = '6vw' height = '6vw'>
+                            <svg width = '100%' height = '100%' viewbox = '0, 0, 100, 100' transform-origin = 'center'>
+                                <rect width = '69%' height = '69%' fill = '#f60914' transform = "rotate(45, -15, 5)" transform-origin = 'center'/>
+                                {/* <rect width = '90%' height = '90%' fill = '#f95961' textAlign = 'center'/> */}
+                                <text x = '50%' y = '50%' textAnchor = 'middle' dominantBaseline = 'middle' fontSize = '2vw' fill = 'black'>
+                                    Today
+                                </text>
+                            </svg>
+                        </Box>
+                        <Stack spacing = {1} divider = {<Divider variant='middle'/>} sx={{width: 1}}>
+                            <Box sx={{display:'flex', justifyContent:'space-between', width: 1, fontSize: '0.8rem'}}>
+                                <Typography variant='body2' sx = {{fontSize: '2.75vw'}}>
+                                    DO LAUNDRY
+                                </Typography>
+                                <Avatar alt = "Espurr again" src = {picture}/>
+                            </Box>
+                            
+                            <Box sx={{display:'flex', justifyContent:'space-between', width: 1}}>
+                                <Typography variant='body2' sx={{color: '#d3d3d3', fontSize: '2vw'}} >
+                                    Use vinegar or sumn, idkadiufhasi how to all i wanna do is do you right, wish that i could climb the greatest heights
+                                </Typography>
+                                <Avatar alt = "Espurr again" src = {picture}/>
+                            </Box>
+                        </Stack>
+                    </ListItem>
+                    <ListItem sx = {{borderColor:'#000000', border: '1px solid black'}}>
                         <ListItemAvatar>
                             <Avatar alt ="Espurr" src = {picture}/>
                         </ListItemAvatar>
