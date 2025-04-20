780ac6a8b1d776f64992229dff0efc964475cf43
Matthew Danque <mattdanque@gmail.com>
2025-04-19 22:42:40 -0500

    added template topbar for submit task

--- src/pages/submitTask.js
+++ src/pages/submitTask.js
@@ -1,10 +1,24 @@
-
+import {AppBar, Box, Container, List, ListItemText, ListItemButton, Divider, Toolbar, 
+    IconButton, Icon, SvgIcon, ListItem, ListItemAvatar, Avatar, Stack, Typography} from "@mui/material";
+import {ArrowBack, Settings} from '@mui/icons-material';
 
 const SubmitTask = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton size="large" edge="start" >
+                            < ArrowBack />
+                        </IconButton>
+                        <Typography variant="h5" sx={{flexGrow:1}}>Confirm Submit Task</Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Container sx={{flexGrow:1}}>
+                
+            </Container>
+        </>
     );
 }
 
